e9edb2734eb42dce2fdd2951014f7339c8fc6479
mahesh7802 <bandimahesh7802@gmail.com>
2023-06-01 18:08:52 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mahesh(request):
+    return render(request,'mahesh.html')
+
+
+def ramesh(request):
+    return render(request,'ramesh.html')
